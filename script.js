a7de9471158904468efea287e6e9c5242bdf9a89
buffaloquicklube <buffaloquicklube@gmail.com>
2026-06-10 00:47:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ document.addEventListener("click", function (event) {
     menu.classList.remove("show");
   }
 });
+
+function initializeOtherFields() {
+  document.querySelectorAll(".conditional-other").forEach(function (field) {
+    if (field.dataset.otherInitialized === "true") {
+      return;
+    }
+
+    var select = document.getElementById(field.getAttribute("data-other-for"));
+    var input = field.querySelector("input");
+
+    if (!select || !input) {
+      return;
+    }
+
+    field.dataset.otherInitialized = "true";
+
+    function updateOtherField() {
+      var selectedText = select.options[select.selectedIndex] ? select.options[select.selectedIndex].text : "";
+      var shouldShow = select.value === "Other" || selectedText === "Other";
+      field.classList.toggle("show", shouldShow);
+      input.required = shouldShow;
+
+      if (!shouldShow) {
+        input.value = "";
+      }
+    }
+
+    select.addEventListener("change", updateOtherField);
+    updateOtherField();
+  });
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", initializeOtherFields);
+} else {
+  initializeOtherFields();
+}
